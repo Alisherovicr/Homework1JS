1453fd574b6491924295167d434a14e64e0e5e01
Akbar <Akbar@com>
2022-05-29 12:44:48 +0300

    lesson1

---
+++ main.js
@@ -0,0 +1,19 @@
+var Name = prompt('Введите ваше имя')
+console.log('Меня зовут ' + Name)
+
+var age = +prompt('Введите ваш возраст')
+console.log('Мне '+ age + ' лет');
+
+var srt = prompt('Решите пример 12+6')
+console.log('Ваш ответ:' + srt +' правильный ответ:18');
+
+var srt = prompt('Решите пример 12-6')
+console.log('Ваш ответ:' + srt +' правильный ответ:6');
+
+var srt = prompt('Решите пример 12*6')
+console.log( 'Ваш ответ:' + srt +' правильный ответ:72');
+
+var srt = prompt('Решите пример 12/6')
+console.log( 'Ваш ответ:' + srt +' правильный ответ:2');
+
+var end = alert('Откройте консоль')
